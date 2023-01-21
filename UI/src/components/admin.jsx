eab08b9b23c6e9b3cba3e1f9cbfae3e76489a94c
Shivaji Jadhav <shivajijadhav5242@gmail.com>
2023-01-21 23:20:29 +0530

    admin_css

--- UI/src/components/admin.jsx
+++ UI/src/components/admin.jsx
@@ -1,4 +1,11 @@
+
 import { useFormik } from "formik";
+import "./admin.css"
+// import "./subscription form.css"
+
+
+
+
 
 function Admin() {
   const formik = useFormik({
@@ -8,34 +15,36 @@ function Admin() {
     },
   });
   return (
-    <div className="App">
+    <div className="App1">
       <h1>Admin</h1>
       <form onSubmit={formik.handleSubmit}>
-        <div>
-        <label>Mail Subject:</label>
+        <div className="form2">
+        {/* <label id="placeholder">Mail Subject:</label> */}
+        <h3>Mail Subject:</h3>
         <input
           name="Mailsubject"
           type={"text"}
           onChange={formik.handleChange}
           value={formik.values.Mailsubject}
-          placeholder="subject"
-          className="input-box"
+          placeholder="Subject"
+          className="input2"
         ></input>
         </div>
         <div>
-<label>Mail Content:</label>
+{/* <label >Mail Content:</label> */}
+<h3>Mail Content:</h3>
         <textarea
           name="Mailcontent"
           type={"textarea"}
           onChange={formik.handleChange}
           value={formik.values.Mailcontent}
           placeholder="Write your content here..."
-          className="input-box"
+          className="input2"
         ></textarea>
           </div>
           <div>
-        <button type="submit" className="admin-btn">
-          Send
+        <button type="submit" className="submit">
+          Send Subject
         </button>
         </div>
       </form>
